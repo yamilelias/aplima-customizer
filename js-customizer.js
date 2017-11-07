be5d81a560fabe0f867781d03c5cb1140dbee3da
yamilelias <yamileliassoto@gmail.com>
2017-11-06 22:19:20 -0700

    Added some documentation and deleted legacy code

--- js-customizer.js
+++ js-customizer.js
@@ -5,39 +5,19 @@
 /*
  * Set the sidebar menu to a certain height depending on the position of the page
  */
-
-// If it already passed the title, then put it in the top
 jQuery(document).scroll(function () {
     var pos = jQuery(window).scrollTop();
-    console.log(pos);
-    // if (pos == 0) {
-    //     alert('top of the div');
-    // }
 
+    // If it already passed the title, then put it in the top
     if (pos > 350) {
         jQuery("#nav_menu-3").animate({
             'top': '5em'
         }, 100);
-    } else {
+    }
+    // If it came back, return it so it won't crash with title!
+    else {
         jQuery("#nav_menu-3").animate({
             'top': '25em'
         }, 100);
     }
 });
-
-// If it returns to the top, then move the menu a little bit
-// jQuery(document).scroll(function () {
-//         if (jQuery(window).height() + jQuery(this).scrollTop() < 1200) {
-//         jQuery("#nav_menu-3").animate({
-//             'top': '25em'
-//         }, 1500);
-//     }
-// });
-
-// jQuery('#nav_menu-3').scroll(function() {
-//     var pos = jQuery('#my_div').scrollTop();
-//     console.log(pos);
-//     if (pos == 0) {
-//         alert('top of the div');
-//     }
-// });
